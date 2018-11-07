96974722bb6d2c5c3adadc338428db4955f6bc7c
Rakibul Alam <limonrakib05690@gmail.com>
2018-11-08 02:57:38 +0600

    Design font-end design in this project

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -4,8 +4,7 @@
         <p class="float-right">
             <a href="#">Back to top</a>
         </p>
-        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-        <p>New to Bootstrap? <a href="../../..">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
+        <p class="text-center" > All right reverse &copy; <?php echo date('Y'); ?></p>
     </div>
 </footer>
 
